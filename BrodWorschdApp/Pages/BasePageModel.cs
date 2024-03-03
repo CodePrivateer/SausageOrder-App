4faaff6baf5f4786f9782eee00996d684d500594
CodePrivateer <o.franck@online.de>
2024-03-03 19:04:45 +0100

    Neue Methode um immer den € als Währung zu haben

--- BrodWorschdApp/Pages/BasePageModel.cs
+++ BrodWorschdApp/Pages/BasePageModel.cs
@@ -18,5 +18,22 @@ namespace BrodWorschdApp.Pages
             ProductList = new List<ProductsTable>();
             ErrorMessage = string.Empty;
         }
+        public static string FormatAsEuro(float? valueFloat = null, decimal? valueDecimal = null)
+        {
+            if (valueFloat.HasValue)
+            {
+                decimal decimalValue = (decimal)valueFloat.Value;
+                return decimalValue.ToString("C", new System.Globalization.CultureInfo("de-DE"));
+            }
+            else if (valueDecimal.HasValue)
+            {
+                decimal decimalValue = (decimal)valueDecimal.Value;
+                return decimalValue.ToString("C", new System.Globalization.CultureInfo("de-DE"));
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
     }
 }
