53b7956a1037c6649914f6dd9f22a0a05fbbff1d
CodePrivateer <o.franck@online.de>
2024-03-30 09:45:59 +0100

    Update Index.cshtml.cs: remove unnecessary if-else conditions, simplify code, add HandleOrderDetails method; update brodworschdapp\_db.db.
    The changes in Index.cshtml.cs include removing unnecessary if-else conditions, simplifying the code, and adding a new HandleOrderDetails method to handle order details loading. The commit also includes an updated version of brodworschdapp\_db.db.

--- BrodWorschdApp/Pages/Index.cshtml.cs
+++ BrodWorschdApp/Pages/Index.cshtml.cs
@@ -18,35 +18,21 @@ namespace BrodWorschdApp.Pages
             if (isOrderViewVisible == "True")
             {
                 IsOrderViewVisible = false;
-            }else if (isOrderViewVisible == "False")
+            }
+            else if (isOrderViewVisible == "False")
             {
                 IsOrderViewVisible = true;
-            }else if(string.IsNullOrEmpty(isOrderViewVisible)) 
+            }
+            else if (string.IsNullOrEmpty(isOrderViewVisible))
             {
                 IsOrderViewVisible = !IsOrderViewVisible;
             }
-
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-
-            if (IsOrderViewVisible)
-            {
-                OrderDetails = await GetOrderDetails(orderNumber);
-            }
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await GetSums();
+            // Orderdetails Laden und Post der Seite
+            await HandleOrderDetails(customerId, orderNumber, IsOrderViewVisible);
         }
 
         public async Task OnPostBookOrder(int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-
             // Erstellen Sie eine Liste der Produkte, die noch nicht gebucht sind
             var unbookedProducts = await _databaseHandler.GetDataFromTable<CustomerOrdersTable>(o => o.OrderNumber == orderNumber && o.Booked != "booked");
 
@@ -55,134 +41,75 @@ namespace BrodWorschdApp.Pages
 
             // Die Bestellmengen vom Lagerinhalt pro Produkt abziehen
             await UpdateInventoryAfterBooking(unbookedProducts);
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
 
         public async Task OnPostBookOrderItem(int productId, int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-
             // Die Bestellung auf gebucht setzen
             await _databaseHandler.UpdateOrderItemStatus(orderNumber, productId);
             // Die Bestellmengen vom Lagerinhalt pro Produkt abziehen
             await UpdateInventory(orderNumber, productId, true);
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
         public async Task OnPostPayOrderItem(int productId, int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-
             // Die Bestellung auf bezahlt setzen
             await _databaseHandler.UpdateOrderItemPayStatus(orderNumber, productId);
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
         public async Task OnPostStornoPayOrderItem(int productId, int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-
             // Die Bestellung auf bezahlt setzen
             await _databaseHandler.UpdateOrderItemPayStatus(orderNumber, productId, "");
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
         public async Task OnPostStornoOrderItem(int productId, int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-
             // Die Bestellung auf storniert setzen
             await _databaseHandler.UpdateOrderItemStatus(orderNumber, productId, "");
             // Die Bestellmengen zum Lagerinhalt pro Produkt hinzuf�gen
             await UpdateInventory(orderNumber, productId, false);
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
 
         public async Task OnPostStornoOrder(int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
             // Die Bestellung auf ungebucht setzen
             await _databaseHandler.UpdateDataInTable<CustomerOrdersTable>(o => o.OrderNumber == orderNumber, entity => entity.Booked = string.Empty);
             // Die Bestellmengen zum Lagerinhalt pro Produkt hinzuz�hlen
             await UpdateInventoryAfterCancellation(orderNumber);
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
         public async Task OnPostPayOrder(int customerId, string orderNumber)
         {
-            CustomerId = customerId;
-            OrderNumber = orderNumber;
-            OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-
             // Die Bestellung auf gebucht setzen
             await _databaseHandler.UpdateDataInTable<CustomerOrdersTable>(o => o.OrderNumber == orderNumber, entity => entity.Paid = "paid");
-
-            OrderDetails = await GetOrderDetails(orderNumber);
-
-            CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
-            ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
-
-            await OnGetAsync();
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
         }
         public async Task OnPostStornoPaidOrder(int customerId, string orderNumber)
+        {
+            // Die Bestellung auf ungebucht setzen
+            await _databaseHandler.UpdateDataInTable<CustomerOrdersTable>(o => o.OrderNumber == orderNumber, entity => entity.Paid = string.Empty);
+            // Orderdetails Laden zum Post der Seite
+            await HandleOrderDetails(customerId, orderNumber);
+        }
+
+        public async Task HandleOrderDetails(int customerId, string orderNumber, bool isOrderViewVisible = true)
         {
             CustomerId = customerId;
             OrderNumber = orderNumber;
             OrderStatus = "Anzeigen";
-            IsOrderViewVisible = true;
-            // Die Bestellung auf ungebucht setzen
-            await _databaseHandler.UpdateDataInTable<CustomerOrdersTable>(o => o.OrderNumber == orderNumber, entity => entity.Paid = string.Empty);
+            IsOrderViewVisible = isOrderViewVisible;
 
             OrderDetails = await GetOrderDetails(orderNumber);
-
             CustomerList = await _databaseHandler.GetDataFromTable<CustomersTable>(x => x.ID == CustomerId);
             ProductList = await _databaseHandler.GetDataFromTable<ProductsTable>(x => true);
 
