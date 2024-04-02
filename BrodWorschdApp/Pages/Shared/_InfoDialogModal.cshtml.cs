291a82dc5bbddeec17ada1b194545ae1f7fa86cd
CodePrivateer <o.franck@online.de>
2024-04-02 20:19:56 +0200

    Add InfoModalViewModel to _InfoDialogModal.cshtml and _InfoDialogModal.cshtml.cs

---
+++ BrodWorschdApp/Pages/Shared/_InfoDialogModal.cshtml.cs
@@ -0,0 +1,10 @@
+namespace BrodWorschdApp
+{
+    public class InfoModalViewModel
+    {
+        public string ModalId { get; set; }
+        public string InformationTitel { get; set; }
+        public string InformationText { get; set; }
+        public string Confirmation { get; set; }
+    }
+}
